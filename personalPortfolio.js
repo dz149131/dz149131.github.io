037572f7f9591ffa7b09130fa3ec7d9d3c2599b5
Danny Zhang <dz149131@gmail.com>
2023-01-16 16:37:54 -0600

    added Copy Email button

--- personalPortfolio.js
+++ personalPortfolio.js
@@ -22,3 +22,13 @@ window.addEventListener('scroll', changeOpacity);
 //Background Move
 var background = document.getElementById("container");
 background.style.animation = "move-background 20s linear infinite";
+
+//Email button
+function copyEmail() {
+    const email = "dz149131@gmail.com";
+    navigator.clipboard.writeText(email).then (function () {
+        alert("Email address copied to clipboard.");
+    }, function(err) {
+        console.error("Failed to copy email:", err)
+    });
+}
